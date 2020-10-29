518d932f3aced113515ef0752b20ac25f9640dcf
Pták Zdeněk <zdenek.ptak@assecosol.com>
2020-10-29 18:21:53 +0100

    Program pro automaticke zvysovani verze

---
+++ commitSvn.bat
@@ -0,0 +1,25 @@
+@echo off
+title Aktualizace zdrojaku HELIOS ORANGE
+
+echo SVN UPDATE (aktualizace ze SVN na lokal)
+
+set osetrenichyby=IF %ERRORLEVEL% EQU 1 msg %username% Chyba pri aktualizaci zdroju!
+
+echo.
+echo.
+echo.
+echo ===== ostre zdroje ========================================
+"c:\Program Files\TortoiseSVN\bin\svn.exe" info https://svn-cz.asol.local/svn/HeliosOrange/heo/trunk/asseco
+echo.
+"c:\Program Files\TortoiseSVN\bin\svn.exe" commit C:\HeO_vyroba_distribuce\Zdroje\Ostre\asseco\_Verze - kopie.inc -m "Automatické zvýšení verze pro noèní buildy: 0986"
+echo.
+echo.
+echo.
+echo.
+
+echo.
+echo automaticke pozdrzeni davky na 30 sec 
+choice /C A /D A /T 30 > nul
+
+
+exit
---
+++ updateSvnFreeze.bat
@@ -0,0 +1,22 @@
+@echo off
+title Aktualizace zdrojaku HELIOS ORANGE
+
+echo SVN UPDATE (aktualizace ze SVN na lokal)
+
+set osetrenichyby=IF %ERRORLEVEL% EQU 1 msg %username% Chyba pri aktualizaci zdroju!
+
+echo.
+echo.
+echo.
+echo ===== ostre zdroje ========================================
+"c:\Program Files\TortoiseSVN\bin\svn.exe" info https://svn-cz.asol.local/svn/HeliosOrange/heo/trunk/asseco
+echo.
+"c:\Program Files\TortoiseSVN\bin\svn.exe" update C:\HeO_vyroba_distribuce\Zdroje\Freeze
+  %osetrenichyby%
+echo.
+echo.
+echo.
+echo.
+
+
+exit
